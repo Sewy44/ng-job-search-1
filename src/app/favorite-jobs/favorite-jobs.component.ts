e5eee5f0d8463c3c8c42ecf5dfde12aba8ebdb3a
Jamie Seward <jamie.seward@gmail.com>
2024-08-05 01:59:45 -0600

    Added favorite jobs component

---
+++ src/app/favorite-jobs/favorite-jobs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-favorite-jobs-view',
+    templateUrl: './favorite-jobs.component.html',
+    styleUrl: './favorite-jobs.component.css'
+})
+export class FavoriteJobsComponent {
+    
+}
